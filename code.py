377c0238ef9d1dc0f81e8b1116f8e97225e22d9a
Arthur LAURENT <laurent.art@gmail.com>
2024-07-03 21:55:27 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,27 @@
+import time
+import board
+import busio
+import usb_midi
+
+# Initialize UART MIDI
+uart = busio.UART(board.GP0, board.GP1, baudrate=31250)
+
+# Initialize USB MIDI output
+midi_out = usb_midi.ports[1]
+
+# Initialize USB MIDI input
+midi_in = usb_midi.ports[0]
+
+while True:
+    try:
+        if uart.in_waiting > 0:
+            # Send received MIDI message from UART to computer via USB MIDI
+            midi_out.write(uart.read(uart.in_waiting))
+            
+        msg = midi_in.read()
+        if msg:
+            # Send received MIDI message from USB MIDI to external device via UART
+            uart.write(msg)
+
+    except Exception as e:
+        print("An error occurred:", e)
